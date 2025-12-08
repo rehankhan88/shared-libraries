a724f43f74425f7410d786acee9e3e2c26822c27
rehankhan88 <rehan9732088@gmail.com>
2025-12-07 21:32:36 -0800

    Update docker_compose.groovy

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -1,3 +1,8 @@
 def call(){
-  sh "docker-compose down && docker-compose up -d"
+  
+  sh '''
+          docker stop notes-app 2>/dev/null || true
+          docker rm notes-app 2>/dev/null || true
+          docker compose up -d
+        '''
 }
