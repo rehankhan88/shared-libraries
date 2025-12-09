78bef8ba23e774a975c2cda6359ad91cd2faf691
rehankhan88 <rehan9732088@gmail.com>
2025-12-08 16:42:37 -0800

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,10 @@
 // Define function
 def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+  // sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+  sh '''#!/bin/bash
+      set -euo pipefail
+      # ensure .dockerignore exists to avoid sending DB files
+      docker build -t notes-app:latest .
+      docker image ls --format '{{.Repository}}:{{.Tag}}' | grep -q '^notes-app:latest$'
+    '''
 }
